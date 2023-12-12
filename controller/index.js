fd3a2c0d1f8622c40e396429a0ef4b92d1f7544d
songsh927 <songsh96927@gmail.com>
2023-12-12 12:37:22 +0900

    user init

--- controller/index.js
+++ controller/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dayjs from 'dayjs';
+import userController from './user/index.js';
 
 const router = express.Router();
 
@@ -7,5 +8,7 @@ router.get('/', (req, res, next) => {
     const _currentDt = dayjs().format('YYYY-MM-DD HH:mm:ss');
     res.status(200).json({message: `${_currentDt} Hello World`});
 })
+
+router.use('/user', userController);
 
 export default router;
