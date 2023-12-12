fcab9708941150ac7175280c0fad072d2f016fe0
songsh927 <songsh96927@gmail.com>
2023-12-12 12:34:31 +0900

    admin init

--- controller/index.js
+++ controller/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dayjs from 'dayjs';
+import adminController from './admin/index.js';
 
 const router = express.Router();
 
@@ -7,5 +8,7 @@ router.get('/', (req, res, next) => {
     const _currentDt = dayjs().format('YYYY-MM-DD HH:mm:ss');
     res.status(200).json({message: `${_currentDt} Hello World`});
 })
+
+router.use('/admin', adminController);
 
 export default router;
